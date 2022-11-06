754e9a8e28a289f4c841979b86708847dacb16db
PatrickkMo <87035456+PatrickkMo@users.noreply.github.com>
2022-11-07 02:48:11 +0800

    thirdcommit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+
 import './App.css';
 
-function App() {
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+function BigText({ text , className = ""}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h className = {className}>{text}</h>
   );
 }
 
-export default App;
+function Root() {
+
+  const [scroll, setscroll] = useState(0);
+  console.log("Scroll" , scroll)
+
+ 
+ 
+
+ 
+
+
+  window.addEventListener("scroll", function(event) {
+    var scroll = this.scrollY
+    setscroll(scroll)
+
+    var item = ((getComputedStyle(document.documentElement).getPropertyValue('--font-size'))).slice(0,4);
+  
+    
+
+    setscroll(scroll)
+    console.log( (parseInt(item) + parseInt(scroll))+"%")
+    document.documentElement.style.setProperty('--font-size', (parseInt(item) + parseInt(scroll)/1000)+"%");
+  })
+
+
+  return (
+   
+  
+
+    <div className="App">
+
+      <header className="App-header">
+       
+      <BigText text = {"PATRICK MO"} className = "name"></BigText>
+
+      <BigText text = {"ENTREPRENEUR"} className = "entre"></BigText>
+
+      </header>
+
+      <p2 className="p2">Hello</p2>
+
+    </div>
+
+   
+  );
+}
+
+export default Root;
